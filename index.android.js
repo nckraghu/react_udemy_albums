e054f584c57dc13eb7a3b03f583e227be48b6c2e
Kalyan Raghu <kalyan.raghu@gmail.com>
2017-04-23 02:54:01 +0530

    Added Hello World

--- index.android.js
+++ index.android.js
@@ -3,3 +3,12 @@
 //Import a library to help create a Component
 import React from 'react';
 import ReactNative from 'react-native';
+
+//Create a Component
+const App = () => {
+  return (
+    <ReactNative.Text>Hello World!</ReactNative.Text>
+  );
+};
+
+ReactNative.AppRegistry.registerComponent('albums', () => App);
