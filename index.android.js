fe73e2af5abc6c38c8e128f5e85d6c1ad8fa9696
Kalyan Raghu <kalyan.raghu@gmail.com>
2017-04-23 02:54:56 +0530

    Smarter import - only what is required

--- index.android.js
+++ index.android.js
@@ -2,13 +2,13 @@
 
 //Import a library to help create a Component
 import React from 'react';
-import ReactNative from 'react-native';
+import { Text, AppRegistry } from 'react-native';
 
 //Create a Component
 const App = () => {
   return (
-    <ReactNative.Text>Hello World!</ReactNative.Text>
+    <Text>Hello World!</Text>
   );
 };
 
-ReactNative.AppRegistry.registerComponent('albums', () => App);
+AppRegistry.registerComponent('albums', () => App);
