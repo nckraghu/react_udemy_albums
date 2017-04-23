beacc70b687ccc0a0b45a28dec80f70f35cf6a87
Kalyan Raghu <kalyan.raghu@gmail.com>
2017-04-23 13:45:37 +0530

    Added fontSize style to header

--- src/components/header.js
+++ src/components/header.js
@@ -4,8 +4,16 @@ import { Text } from 'react-native'
 
 //Make a Component
 const Header = () => {
-  return <Text>Albums!</Text>
+  const { textStyle } = styles;
+
+  return <Text style={textStyle}>Albums!</Text>
 }
+
+const styles = {
+  textStyle: {
+    fontSize: 20
+  }
+};
 
 //Make the component available to other parts of the app
 export default Header;
