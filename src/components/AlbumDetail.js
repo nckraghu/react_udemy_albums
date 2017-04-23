f50e88ed75924199281017500c1b94d3b0e52468
Kalyan Raghu <kalyan.raghu@gmail.com>
2017-04-24 02:04:32 +0530

    CardSection styling in AlbumDetail.js

--- src/components/AlbumDetail.js
+++ src/components/AlbumDetail.js
@@ -1,6 +1,6 @@
 //Import libraries for components
 import React from 'react';
-import { Text } from 'react-native';
+import { Text, View, Image } from 'react-native';
 import Card from './Card';
 import CardSection from './CardSection';
 
@@ -9,10 +9,27 @@ const AlbumDetail = (props) => {
   return (
     <Card>
       <CardSection>
-        <Text>{props.album.title}</Text>
+        <View>
+          <Image source={{ uri: props.album.thumbnail_image }} style={styles.thumbnailStyle}/>
+        </View>
+        <View style={styles.headerContentStyle}>
+          <Text>{props.album.title}</Text>
+          <Text>{props.album.artist}</Text>
+        </View>
       </CardSection>
     </Card>
   );
+};
+
+const styles = {
+  headerContentStyle: {
+    flexDirection: 'column',
+    justifyContent: 'space-around'
+  },
+  thumbnailStyle: {
+    height: 50,
+    width: 50
+  }
 };
 
 //export AlbumDetail
