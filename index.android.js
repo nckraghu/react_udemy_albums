5fd17b468783443741621b9fc293ea12f8b29d4a
Kalyan Raghu <kalyan.raghu@gmail.com>
2017-04-23 13:34:52 +0530

    Added Header to index file

--- index.android.js
+++ index.android.js
@@ -2,11 +2,12 @@
 
 //Import a library to help create a Component
 import React from 'react';
-import { Text, AppRegistry } from 'react-native';
+import { AppRegistry } from 'react-native';
+import Header from './src/components/header'
 
 //Create a Component
 const App = () => (
-    <Text>Hello World!</Text>
+    <Header />
   );
 
 AppRegistry.registerComponent('albums', () => App);
---
+++ src/components/header.js
@@ -0,0 +1,11 @@
+//Import libraries
+import React from 'react'
+import { Text } from 'react-native'
+
+//Make a Component
+const Header = () => {
+  return <Text>Albums!</Text>
+}
+
+//Make the component available to other parts of the app
+export default Header;
