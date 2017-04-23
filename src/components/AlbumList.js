279c830a04154fd407330995b2a73a5abaf79762
Kalyan Raghu <kalyan.raghu@gmail.com>
2017-04-23 17:59:18 +0530

    AlbumList component created and added to App

---
+++ src/components/AlbumList.js
@@ -0,0 +1,14 @@
+//Import libraries
+import React from 'react';
+import { View, Text } from 'react-native'
+
+//Creating the Component
+const AlbumList = () => {
+  return (
+      <View>
+        <Text>Album List</Text>
+      </View>
+  );
+};
+
+export default AlbumList;
