0871e7e3caa9d33b529edd15db0d078a00c5d1f5
Kalyan Raghu <kalyan.raghu@gmail.com>
2017-04-25 02:59:51 +0530

    Added border to Album thumbnail

--- src/components/AlbumDetail.js
+++ src/components/AlbumDetail.js
@@ -9,11 +9,15 @@ const AlbumDetail = ({ album }) => {
   //destructuring album
   const { title, artist, thumbnail_image } = album;
   //destructuring styles
-  const { headerContentStyle, thumbnailStyle } = styles;
+  const {
+    headerContentStyle,
+    thumbnailStyle,
+    thumbnailContainerStyle
+   } = styles;
   return (
     <Card>
       <CardSection>
-        <View>
+        <View style={thumbnailContainerStyle}>
           <Image source={{ uri: thumbnail_image }} style={thumbnailStyle} />
         </View>
         <View style={headerContentStyle}>
@@ -33,6 +37,12 @@ const styles = {
   thumbnailStyle: {
     height: 50,
     width: 50
+  },
+  thumbnailContainerStyle: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginLeft: 10,
+    marginRight: 10
   }
 };
 
